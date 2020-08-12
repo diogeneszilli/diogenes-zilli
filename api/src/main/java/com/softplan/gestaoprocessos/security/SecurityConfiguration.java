3191ae40424da79c1cfa780ce1b75cb73cccb453
Diógenes Zilli <diogeneszilli@gmail.com>
2020-08-12 18:10:39 -0300

    Added security configurations

---
+++ api/src/main/java/com/softplan/gestaoprocessos/security/SecurityConfiguration.java
@@ -0,0 +1,31 @@
+package com.softplan.gestaoprocessos.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder authenticationMgr) throws Exception {
+        authenticationMgr.inMemoryAuthentication()
+                .withUser("admin").password("$2a$10$L871JbehyNU.TCnFCfR75ucfjtHE8THmO5yziXrgZr3rDa4THm1Pq").roles("ADMINISTRADOR");
+    }
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().antMatchers("/h2-console/**");
+    }
+}
