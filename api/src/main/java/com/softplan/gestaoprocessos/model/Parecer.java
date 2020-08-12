40ffdfc09c123df931ab1163955d0eb95ba07b12
Diógenes Zilli <diogeneszilli@gmail.com>
2020-08-12 10:24:37 -0300

    Added Processo and Parecer models

---
+++ api/src/main/java/com/softplan/gestaoprocessos/model/Parecer.java
@@ -0,0 +1,60 @@
+package com.softplan.gestaoprocessos.model;
+
+import com.sun.istack.NotNull;
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Data
+@Entity
+@Table(name = "parecer")
+@SequenceGenerator(name = "parecer_seq", allocationSize = 1)
+public class Parecer {
+
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(generator = "parecer_seq", strategy = GenerationType.SEQUENCE)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "id_usuario")
+    @NotNull
+    private Usuario usuario;
+
+    @Column(name = "descricao")
+    private String descricao;
+
+    @Column(name = "pendente")
+    @NotNull
+    private Boolean pendente;
+
+    public static class Builder {
+
+        private Parecer parecer;
+
+        public Builder() {
+            parecer = new Parecer();
+        }
+
+        public Builder usuario(Usuario usuario) {
+            parecer.setUsuario(usuario);
+            return this;
+        }
+
+        public Builder descricao(String descricao) {
+            parecer.setDescricao(descricao);
+            return this;
+        }
+
+        public Builder pendente(Boolean pendente) {
+            parecer.setPendente(pendente);
+            return this;
+        }
+
+        public Parecer build() {
+            return parecer;
+        }
+
+    }
+
+}
