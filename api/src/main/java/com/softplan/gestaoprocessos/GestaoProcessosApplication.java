fe6d4926af9b3203fd2c68400a4c606b8b4eb297
Diógenes Zilli <diogeneszilli@gmail.com>
2020-08-11 21:12:37 -0300

    Spring web server initial configuration

---
+++ api/src/main/java/com/softplan/gestaoprocessos/GestaoProcessosApplication.java
@@ -0,0 +1,13 @@
+package com.softplan.gestaoprocessos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestaoProcessosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestaoProcessosApplication.class, args);
+	}
+
+}
---
+++ api/src/test/java/com/softplan/gestaoprocessos/GestaoProcessosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.softplan.gestaoprocessos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestaoProcessosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
