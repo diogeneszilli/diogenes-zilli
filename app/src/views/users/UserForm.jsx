b1bc9f538415955975dee362147fa1bc46993d0a
Diógenes Zilli <diogeneszilli@gmail.com>
2020-08-14 14:59:10 -0300

    Added create and update users methods

--- app/src/views/users/UserForm.jsx
+++ app/src/views/users/UserForm.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { NavLink } from "react-router-dom";
 import { Grid, Row, Col } from "react-bootstrap";
+import api from "services/api";
 
 import { Card } from "components/Card/Card.jsx";
 import { FormInputs } from "components/FormInputs/FormInputs.jsx";
@@ -8,14 +9,86 @@ import Button from "components/CustomButton/CustomButton.jsx";
 
 class UserForm extends Component {
 
+  state = {
+    user: {
+      name: "",
+      password: "",
+      roles: [{
+        id: 1,
+        role: "ADMINISTRADOR"
+      }],
+    }
+  }
+
   async componentDidMount() {
     const { id } = this.props.match.params;
-    console.log('edit id', id)
-}
+    if (!!id) {
+      this.loadUser(id);
+    }
+  }
+
+  refreshPage() {
+    window.location.reload(false);
+  }
+
+  async loadUser(id) {
+    const { data } = await api.get(`usuarios/${id}`);
+    this.setState({ user: data })
+  }
+
+  async saveUser(user) {
+    if (!!user.id) {
+      await api.put(`usuarios/${user.id}`, user);
+    } else {
+      await api.post(`usuarios`, user);
+      this.refreshPage();
+    }
+  }
+
+  changeName(e) {
+    let { user } = this.state;
+    user.name = e.target.value;
+    this.setState({ user: user })
+  }
+
+  changePassword(e) {
+    let { user } = this.state;
+    user.password = e.target.value;
+    this.setState({ user: user })
+  }
+
+  changeRole(e) {
+    let { user } = this.state;
+    user.roles.pop();
+    user.roles.push(this.selectRole(e.target.value));
+    this.setState({ user: user })
+  }
+
+  selectRole(roleName) {
+    let role = {}
+    const ADM = "ADMINISTRADOR";
+    const TRI = "TRIADOR";
+    const FIN = "FINALIZADOR";
+    if (roleName === ADM) {
+      role.id = 1;
+      role.role = ADM;
+    }
+    if (roleName === TRI) {
+      role.id = 2;
+      role.role = TRI;
+    }
+    if (roleName === FIN) {
+      role.id = 3;
+      role.role = FIN;
+    }
+    return role;
+  }
 
   render() {
 
-    const passwordLabel = !!this.props.match.params.id ? "New Password" : "Password";
+    const { user } = this.state;
+    const id = this.props.match.params.id;
+    const passwordLabel = !!id ? "New Password" : "Password";
 
     return (
       <div className="content">
@@ -34,29 +107,41 @@ class UserForm extends Component {
                           type: "text",
                           bsClass: "form-control",
                           placeholder: "Username",
-                          defaultValue: ""
+                          required: true,
+                          defaultValue: user.name,
+                          onChange:(event) => this.changeName(event)
                         },
                         {
                           label: passwordLabel,
                           type: "password",
                           bsClass: "form-control",
                           placeholder: "Password",
-                          defaultValue: ""
+                          required: true,
+                          defaultValue: user.password,
+                          onChange:(event) => this.changePassword(event)
                         }
                       ]}
                     />
-    
                   <label>Role</label>
-                  <select ng-selected="vm.tipoUsuario" ng-model="vm.tipoUsuario" class="form-control" ng-options="i.tipo for i in vm.tiposUsuario track by i._id"></select>
+                  <select required className="form-control" value={user.roles[0].role} onChange={(event) => this.changeRole(event)}>
+                    <option value="ADMINISTRADOR">Administrador</option>
+                    <option value="TRIADOR">Triador</option>
+                    <option value="FINALIZADOR">Finalizador</option>
+                  </select>
                     
                     <NavLink to="/admin/user" activeClassName="active">
-                      <Button bsStyle="danger" pullRight marginLeftTop fill type="submit">
+                      <Button bsStyle="danger" pullRight marginLeftTop fill>
                         Cancelar
                       </Button>
                     </NavLink>
-                    <Button bsStyle="info" pullRight marginLeftTop fill type="submit">
-                    {!!this.props.match.params.id ? "Atualizar" : "Salvar"}
-                    </Button>
+                    <NavLink to="/admin/new/user" activeClassName="active">
+                      <Button 
+                        onClick={() => this.saveUser(user)}
+                        disabled={(!!!this.state.user.name || !!!this.state.user.password)}
+                        bsStyle="info" pullRight marginLeftTop fill>
+                      {!!this.props.match.params.id ? "Atualizar" : "Salvar"}
+                      </Button>
+                    </NavLink>
                     <div className="clearfix" />
                   </form>
                 }
