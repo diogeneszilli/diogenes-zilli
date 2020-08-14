9187b806cce99b1d7183403d149484321a979d53
Diógenes Zilli <diogeneszilli@gmail.com>
2020-08-14 14:38:45 -0300

    Change CascadeType to delete users

--- api/src/main/java/com/softplan/gestaoprocessos/model/User.java
+++ api/src/main/java/com/softplan/gestaoprocessos/model/User.java
@@ -25,7 +25,7 @@ public class User {
     @NotNull
     private String password;
 
-    @ManyToMany(cascade = CascadeType.ALL)
+    @ManyToMany(fetch = FetchType.LAZY, cascade = {CascadeType.PERSIST, CascadeType.MERGE, CascadeType.REFRESH, CascadeType.DETACH})
     @JoinTable(name = "user_role", joinColumns = @JoinColumn(name = "user_id"), inverseJoinColumns = @JoinColumn(name = "role_id"))
     private Set<Role> roles;
 
